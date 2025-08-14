50f9c0cce3c367f56c041d3a3012769a60d01f6b
Naveed Ali Khan <naveedalikhan4177@gmail.com>
2025-08-14 17:38:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'pos-164650439.ap-south-1.elb.amazonaws.com'
+    ]
+  }
 })
